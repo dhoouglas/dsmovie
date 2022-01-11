151207df8447269560a7eb567f22bc23896dd15f
Douglas Araújo <dhoouglas@hotmail.com>
2022-01-11 14:09:52 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,37 @@
+import MovieCard from "../../components/MovieCard";
+import MovieScore from "../../components/MovieScore";
+import MovieStars from "../../components/MovieStars";
 import Pagination from "../../components/Pagination";
 
-function Listing () {
+function Listing() {
 
     return (
-        <Pagination />
-    );
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    
+                </div>
+            </div>
 
+        </>
+
+    );
 }
 
 export default Listing;
